c89705e9e123e0399f1ed67fc5ae250e55e5785e
thdevss <pichet.suesat@gmail.com>
2016-09-17 14:47:43 +0700

    fixed : add lower case for if statement

--- quiz.1.3.rb
+++ quiz.1.3.rb
@@ -52,7 +52,8 @@ US_states =
 	"Wyoming" => "WY"}
 
 def checkAEIOU(char)
-	if char=="a" || char=="e" || char=="i" || char=="o" || char=="u" || char=="A" || char=="E" || char=="I" || char=="O" || char=="U" 
+	char = char.downcase
+	if char=="a" || char=="e" || char=="i" || char=="o" || char=="u" 
 		return true
 	end 
 
